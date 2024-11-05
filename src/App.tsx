e3da6d0b38feb4992d3cefa2387b6299fc71a123
LucianoFedericoC <carduccilucianof@gmail.com>
2024-11-05 18:28:24 -0300

    feat: :sparkles: Register component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Layout from './layout'
-import { Home, ListByCategory, Cart, Login } from './pages'
-import { HOME_ROUTE, LIST_BY_CATEGORY_ROUTE, CART_ROUTE, LOGIN_ROUTE } from './config/routes'
+import { Home, ListByCategory, Cart, Login, Signin } from './pages'
+import { HOME_ROUTE, LIST_BY_CATEGORY_ROUTE, CART_ROUTE, LOGIN_ROUTE, SIGNIN_ROUTE } from './config/routes'
 import Hero from './components/hero'
 
 function App() {
@@ -14,6 +14,7 @@ function App() {
           <Route path={LIST_BY_CATEGORY_ROUTE} element={<ListByCategory />} />
           <Route path={CART_ROUTE} element={<Cart />} />
           <Route path={LOGIN_ROUTE} element={<Login />} />
+          <Route path={SIGNIN_ROUTE} element={<Signin />} />
         </Routes>
       </Layout>
     </Router>
--- src/services/auth/login/POST/login.post.service.ts
+++ src/services/auth/login/POST/login.post.service.ts
@@ -1,13 +1,16 @@
 import { AppApiGateWayNoJWT } from '../../../app.api.gateway-no-jwt'
 import { setLocalStorage } from '../../../../utils/local-storage/setLocalStorage'
-import { ILoginInterface } from './models/login.post.interface'
+import { useNavigate } from 'react-router-dom'
+import { IAuth } from '../../models/auth.interface'
 
-export const login = async (body: ILoginInterface) => {
+export const login = async (body: IAuth) => {
+  const navigate = useNavigate()
   try {
     const response = await AppApiGateWayNoJWT.post('/auth/login', body)
     if (response) {
       const data = response.data
       setLocalStorage('user', data)
+      navigate('/home')
     }
   } catch (error) {
     throw error
