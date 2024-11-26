ca9b7382c71d7b92e8bace53835107d02e9c0c80
LucianoFedericoC <carduccilucianof@gmail.com>
2024-11-26 14:41:57 -0300

    feat: :sparkles: profile's traduction added

--- src/pages/profile/components/ReservationsModal.tsx
+++ src/pages/profile/components/ReservationsModal.tsx
@@ -1,24 +1,26 @@
 import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure } from '@nextui-org/react'
+import { useTranslation } from 'react-i18next'
 
 const ReservationsModal = () => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure()
+  const { t } = useTranslation()
 
   return (
     <>
       <Button onPress={onOpen} className='w-64'>
-        My reservations
+        {t('Profile.reservations.title')}
       </Button>
       <Modal isOpen={isOpen} onOpenChange={onOpenChange} size='2xl'>
         <ModalContent>
           {onClose => (
             <>
-              <ModalHeader className='flex flex-col gap-1'>My reservations</ModalHeader>
+              <ModalHeader className='flex flex-col gap-1'> {t('Profile.reservations.title')}</ModalHeader>
               <ModalBody>
                 <p>Reservations</p>
               </ModalBody>
               <ModalFooter>
                 <Button color='danger' variant='light' onPress={onClose}>
-                  Close
+                  {t('Profile.reservations.close_button')}
                 </Button>
               </ModalFooter>
             </>
